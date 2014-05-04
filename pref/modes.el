e3ad14c96d63c18f24d6c719334ec1a3efc6e75a
htlsne <htlsne@users.noreply.github.com>
2014-05-04 19:00:58 +0900

    LilyPondのプレビューを、Preview.appで出来るように キーバインドも設定

--- pref/modes.el
+++ pref/modes.el
@@ -132,5 +132,14 @@
 
 ;; lilypondモード
 (load "lilypond-init.el")
+;; pdfのプレビュー
+(defun LilyPond-pdf-view ()
+  (interactive)
+  (shell-command
+   (concat "filename=" (buffer-file-name) "; open ${filename%.*}.pdf")))
+;; (define-key LilyPond-mode-map (kbd "C-c C-v") 'LilyPond-pdf-view)
+(add-hook 'LilyPond-mode-hook
+          '(lambda ()
+             (local-set-key (kbd "C-c C-v") 'LilyPond-pdf-view)))
 
 (provide 'modes)
