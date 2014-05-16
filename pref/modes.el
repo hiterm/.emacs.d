fd5010d90ab8ea6cebb9a55993f1bcb69a4240e4
htlsne <htlsne@users.noreply.github.com>
2014-05-16 22:10:38 +0900

    LilyPond-modeの設定についての細かな修正

--- pref/modes.el
+++ pref/modes.el
@@ -1,16 +1,6 @@
 ;; 様々なモードの毎の設定
 
 
-;; ; html-mode
-;; (add-hook 'html-mode-hook
-;; (lambda ()
-;; (setq indent-tabs-mode t)
-;; (setq tab-width 4)))
-
-                                        ; Tclモード
-;; (add-hook 'html-mode-hook '(lambda () (setq tab-width 4)))
-;; (setq c-basic-offset 4)
-
 ;; htmlモードでインデント幅を4に
 (add-hook
  'sgml-mode-hook
@@ -19,12 +9,6 @@
     sgml-basic-offset 4
     )))
 (setq indent-tabs-mode t)
-
-;; ;; htmlモードのオートインデントをやめる
-;; (add-hook 'html-mode-hook
-;;           '(lambda ()
-;;              (global-set-key "\C-m" 'newline)
-;;              ))
 
 ;; asymptoteモード
 (autoload 'asy-mode "asy-mode.el" "Asymptote major mode." t)
@@ -64,14 +48,6 @@
                    sh-indentation 2
                    indent-tabs-mode nil)))
 
-;; ;; fortranモード
-;; (add-hook 'fortran-mode-hook
-;;           '(lambda ()
-;;             (setq fortran-do-indent 4)
-;;             (setq fortran-if-indent 4)
-;;             (setq fortran-structure-indent 4)
-;;             (setq fortran-continuation-indent 4)))
-
 ;; ;; f90モード
 (add-hook 'before-save-hook
           'delete-trailing-whitespace
@@ -80,29 +56,6 @@
           '(lambda () (add-hook 'before-save-hook
                                 'delete-trailing-whitespace
                                 'trim-eob)))
-;; (add-hook 'f90-mode-hook
-;;           '(lambda () (setq f90-do-indent 4
-;;                             f90-if-indent 4
-;;                             f90-type-indent 4
-;;                             f90-program-indent 4
-;;                             f90-continuation-indent 4
-;;                             f90-comment-region "!!$"
-;;                             f90-directive-comment-re "!hpf\\$"
-;;                             f90-indented-comment-re "!"
-;;                             f90-break-delimiters "[-+\\*/,><=% \t]"
-;;                             f90-break-before-delimiters t
-;;                             f90-beginning-ampersand t
-;;                             f90-smart-end 'blink
-;;                             f90-auto-keyword-case nil
-;;                             f90-leave-line-no  nil
-;;                             f90-startup-message t
-;;                             indent-tabs-mode t
-;;                             f90-font-lock-keywords f90-font-lock-keywords-2)))
-;;              ;; ;;The rest is not default.
-;;              ;; (abbrev-mode 1)             ; turn on abbreviation mode
-;;              ;; (f90-add-imenu-menu)        ; extra menu with functions etc.
-;;              ;; (if f90-auto-keyword-case   ; change case of all keywords on startup
-;;              ;;     (f90-change-keywords f90-auto-keyword-case))
 
 ;; Textモード
 ;; 空白を削除しない
@@ -137,9 +90,8 @@
   (interactive)
   (shell-command
    (concat "filename=" (buffer-file-name) "; open ${filename%.*}.pdf")))
-(add-hook 'LilyPond-mode-hook
-          '(lambda ()
-             (local-set-key (kbd "C-c C-v") 'LilyPond-pdf-view)))
+(eval-after-load "lilypond-mode"
+  '(define-key LilyPond-mode-map (kbd "C-c C-v") 'LilyPond-pdf-view))
 ;; <>をsmartparensで補完
 (eval-after-load "LilyPond-mode"
   '(sp-pair "<" ">"))
