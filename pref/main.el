2f24879f94f803e84979e453bedca967f2d8c4a8
htlsne <htlsne@users.noreply.github.com>
2014-05-21 12:09:49 +0900

    recentfで30秒毎に保存するように

--- pref/main.el
+++ pref/main.el
@@ -78,6 +78,37 @@
 ;; recent-files
 (require 'recentf)
 (recentf-mode 1)
+(defvar my-recentf-list-prev nil)
+;; 30秒毎に保存
+(defadvice recentf-save-list
+(around no-message activate)
+"If `recentf-list' and previous recentf-list are equal,
+do nothing. And suppress the output from `message' and
+`write-file' to minibuffer."
+(unless (equal recentf-list my-recentf-list-prev)
+(cl-flet ((message (format-string &rest args)
+(eval `(format ,format-string ,@args)))
+(write-file (file &optional confirm)
+(let ((str (buffer-string)))
+(with-temp-file file
+(insert str)))))
+ad-do-it
+(setq my-recentf-list-prev recentf-list))))
+
+(defadvice recentf-cleanup
+(around no-message activate)
+"suppress the output from `message' to minibuffer"
+(cl-flet ((message (format-string &rest args)
+(eval `(format ,format-string ,@args))))
+ad-do-it))
+
+(setq recentf-save-file (expand-file-name ".recentf" user-emacs-directory))
+(setq recentf-max-saved-items 2000)
+(setq recentf-exclude '(".recentf"))
+(setq recentf-auto-cleanup 10)
+(run-with-idle-timer 30 t 'recentf-save-list)
+(recentf-mode 1)
+
 
 ;; 矩形選択モード (C-Enter)
 (cua-mode t)
