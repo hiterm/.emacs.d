aab8e5fa9940f8f18e94858ff419b30cd622d551
htlsne <htlsne@users.noreply.github.com>
2014-07-11 00:43:03 +0900

    iswitchbのキーバインドを設定

--- pref/main.el
+++ pref/main.el
@@ -239,6 +239,15 @@ ad-do-it))
       (setq iswitchb-buffer-ignore-asterisk-orig nil)
       (iswitchb-make-buflist iswitchb-default)
       (setq iswitchb-rescan t))))
+;;; C-f, C-b, C-n, C-p で候補を切り替えることができるように。
+(add-hook 'iswitchb-define-mode-map-hook
+      (lambda ()
+        (define-key iswitchb-mode-map "\C-n" 'iswitchb-next-match)
+        (define-key iswitchb-mode-map "\C-p" 'iswitchb-prev-match)
+        (define-key iswitchb-mode-map "\C-f" 'iswitchb-next-match)
+        (define-key iswitchb-mode-map "\C-b" 'iswitchb-prev-match)))
+
+
 
 ;; Highlighting indentation for Emacs
 (require 'highlight-indentation)
