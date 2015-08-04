14a2a438f7973593bf52579b2a81745be1a8d385
htlsne <htlsne@users.noreply.github.com>
2015-08-04 11:17:17 +0900

    inertial scrollをやめた

--- pref/gui.el
+++ pref/gui.el
@@ -10,19 +10,6 @@
 ;; 現在行のハイライト
 (global-hl-line-mode)
 
-;; 慣性スクロール
-(require 'inertial-scroll)
-(setq inertias-global-minor-mode-map
-      (inertias-define-keymap
-       '(
-         ;; Mouse wheel scrolling
-         ("<wheel-up>"   . inertias-down-wheel)
-         ("<wheel-down>" . inertias-up-wheel)
-         ("<mouse-4>"    . inertias-down-wheel)
-         ("<mouse-5>"    . inertias-up-wheel)
-         ) inertias-prefix-key))
-(inertias-global-minor-mode 1)
-
 ;; スクロール時のビープ音を消す
 (defun my-bell-function ()
   (unless (memq this-command
