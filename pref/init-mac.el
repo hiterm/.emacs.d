47fb0a0b34a72f7c849c8123a1f8e4c848216db3
htlsne <htlsne@users.noreply.github.com>
2016-11-13 12:33:22 +0900

    Remove proof-general

--- pref/init-mac.el
+++ pref/init-mac.el
@@ -42,7 +42,4 @@
                 (".*monaco-bold-.*-mac-roman" . 0.9)
                 ("-cdac$" . 1.3))))))
 
-;; proofgeneral
-(load-file "/usr/local/share/emacs/site-lisp/ProofGeneral/generic/proof-site.el")
-
 (provide 'init-mac)
